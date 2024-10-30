ddf6487d104e52b2360feb7726a6d76c6b26a00e
Dave Curtis <dave.curtis@focusrite.com>
2024-10-30 21:13:49 +0000

    add LED name to repr

--- pymcu/messages/button.py
+++ pymcu/messages/button.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass, field
+from .hardware_mapping import NOTE_MAP
 
 LED_OFF = 0x00
 LED_BLINK = 0x01
 LED_ON = 0x7F
 
-
 @dataclass
 class SetLED():
     index: int = field()
     state: int = field()
+
+    name: str = None
+
+    def __post_init__(self):
+        self.name = NOTE_MAP.get(self.index, "Unknown")
 
     def encode(self):
         return [0x90, self.index, self.state]
